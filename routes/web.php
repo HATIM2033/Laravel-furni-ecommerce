efdbca3489e854a55fd1c7af76c6ff6b802e3332
=HATIM2033 <=hatimelazzaoui51@gmail.com>
2026-02-06 11:17:27 +0100

    Fix problems

--- routes/web.php
+++ routes/web.php
@@ -88,7 +88,6 @@ Route::get('/explore', [ShopController::class, 'explore'])->name('explore');
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::get('/profile/edit-new', [ProfileController::class, 'edit'])->name('profile.edit-new');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     
